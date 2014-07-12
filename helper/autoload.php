3455f942bd09e9484b227c9e7b0263978e11b55d
Haruo Kinoshita <haruo31@underthetree.jp>
2014-07-13 01:50:33 +0900

    windowsでロードが失敗する問題の修正

--- helper/autoload.php
+++ helper/autoload.php
@@ -5,25 +5,25 @@ spl_autoload_register(function ($class) {
     static $path = NULL;
 
     if ($path === NULL) {
-        $path = dirname(__FILE__) . '/..';
+        $path = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..';
 
         $traverse = function (&$paths, $dir) use ( &$traverse, $path ) {
-            $files = glob($dir . '/*.php');
+            $files = glob($dir . DS . '*.php');
             foreach ($files as $f) {
                 $n = strtolower(
                     str_replace(DIRECTORY_SEPARATOR, '\\', str_replace('.php', '', str_replace($path . DIRECTORY_SEPARATOR, '', $f))));
                 $paths[$n] = $f;
             }
-            $dirs = glob($dir . '/*', GLOB_ONLYDIR);
+            $dirs = glob($dir . DIRECTORY_SEPARATOR . '*', GLOB_ONLYDIR);
             foreach ($dirs as $d) {
                 $traverse($paths, $d);
             }
         };
 
         $classes = array();
-        $traverse($classes, $path . '/client');
-        $traverse($classes, $path . '/api');
-        $traverse($classes, $path . '/configurator');
+        $traverse($classes, $path . DIRECTORY_SEPARATOR . 'client');
+        $traverse($classes, $path . DIRECTORY_SEPARATOR . 'api');
+        $traverse($classes, $path . DIRECTORY_SEPARATOR . 'configurator');
     }
 
     $cn = strtolower(str_replace('LangridSettingClient\\', '', $class));
